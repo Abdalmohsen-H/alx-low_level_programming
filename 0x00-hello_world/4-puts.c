1886e1799c0497d902415597a4c650de702d4679
Abdalmohsen-H <egywebdes@gmail.com>
2023-03-16 09:18:51 -0700

    puts command in C fix betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
 /**
- * puts
+ * main - Entry point
+ *
+ * puts example
+ *
+ * Return: always 0 - success
  */
-int main(void){
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
