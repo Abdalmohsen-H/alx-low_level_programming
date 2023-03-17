1a494791231325f1b884cc3a19a3c9312406f33f
Abdalmohsen-H <egywebdes@gmail.com>
2023-03-16 18:18:02 -0700

    print to the to the standard error+ fix betty

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 /**
  * main - Entry point
  *
- * Return: always 0 - success
+ * Return: always 1 - success
  */
 int main(void)
 {
-    fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
-    return 1;
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
+	return (1);
 }
