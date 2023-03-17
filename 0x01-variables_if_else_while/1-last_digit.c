ba797dfc066376ff6b31a20a59af91f74f120ad6
Abdalmohsen-H <egywebdes@gmail.com>
2023-03-17 16:03:33 -0700

    last digit + absolute value (abs)

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,11 +10,12 @@
 int main(void)
 {
 	int n;
+	int lstdg;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	int lstdg = n%10
+	lstdg = abs(n % 10);
 	if (lstdg > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, lstdg);
 	else if (lstdg == 0)
