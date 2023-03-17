e9cfddbe6e895078e673100be164a1cc6ef4d2bd
Abdalmohsen-H <egywebdes@gmail.com>
2023-03-16 17:47:38 -0700

    sizeof wip fixed betty

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,24 +2,11 @@
 /**
  * maine -Entry point
  *
- * return : always 0 - (success)
+ * return: always 0  - (success)
  *
  */
 int main(void)
 {
-	/*
-	int x = sizeof(char);
-	int y = sizeof(int);
-	int z = sizeof(long int);
-	int q = sizeof(long long int);
-	int flo = sizeof(float);
-	printf("Size of a char: %d byte(s)\n", x);
-	printf("Size of a int: %d byte(s)\n", y);
-	printf("sie of a long int : %d byte (s)\n", (int)sizeof(long int));
-	printf("Size of a long long int: %d byte(s)\n", q);
-	printf("Size of a float: %d byte(s)\n", flo);
-	*/
-
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
