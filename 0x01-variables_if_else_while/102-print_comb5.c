992478827bf7d3bef32c470fe2f6c1c51ccd21c3
Abdalmohsen-H <egywebdes@gmail.com>
2023-03-17 20:01:28 -0700

    f

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,14 +11,14 @@ int sec;
 for (fir = 0; fir <= 99; fir++)
 {
 	for (sec = 0; sec <= 99; sec++)
-	{	
+	{
 		if (fir < sec)
 		{
 			putchar(fir / 10 + '0');
 			putchar(fir % 10 + '0');
 			putchar(' ');
 			putchar(sec / 10 + '0');
-			putchar(sec % 10 + '0');	
+			putchar(sec % 10 + '0');
 			if (fir != 98 || sec != 99)
 			{
 				putchar(',');
