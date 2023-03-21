524bd0bab451af2454d1368c0b6c4e12ecbbc42d
Abdalmohsen-H <egywebdes@gmail.com>
2023-03-21 08:33:50 -0700

    create a header and function prints char

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <main.h>
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	/*this takes the first char of the string*/
+	char *wor = "trest";
+	/**
+	 * another way for this task char sh[] = {'_', 'p', 'u', 't', 'c'
+	 * , 'h', 'a', 'r', '\0'}; where '\0' stand for string ended
+	 */
+	while (*wor)
+	{
+		_putchar(*wor);
+		/* increments pointer variable "wor" to point to next character in string */
+
+		*wor++;
+	}
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,6 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+char _putchar(char xd);
+
+#endif
