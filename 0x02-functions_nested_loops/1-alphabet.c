312d371fc8ba705d0cd7bb2fd0667a4ecf28c4f0
Abdalmohsen-H <egywebdes@gmail.com>
2023-03-21 09:19:17 -0700

    custom function to print lowercase alphabet fix betty

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,11 +8,11 @@
 void print_alphabet(void)
 {
 		char *wor = "abcdefghijklmnopqrstuvwxyz";
+		
 		while (*wor)
 		{
 			_putchar(*wor);
 			/* increments pointer variable "wor" to point to next character in string */
-	
 			wor++;
 		}
 		_putchar('\n');
