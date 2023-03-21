a40a4e028a4de2ba8286a281a883e50bdf6e26be
Abdalmohsen-H <egywebdes@gmail.com>
2023-03-21 09:48:46 -0700

    custom function to print lowercase alphabet 10 times fix bityy

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet - print lowercase alphbet
+ * print_alphabet_10x - print lowercase alphbet
  *
  * Return: Always 0 (Success)
  *
@@ -9,7 +9,7 @@ void print_alphabet_x10(void)
 {
 int i;
 
-for (i=1; i <= 10 ; i++)
+for (i = 1; i <= 10 ; i++)
 {
 	char *wor = "abcdefghijklmnopqrstuvwxyz";
 
