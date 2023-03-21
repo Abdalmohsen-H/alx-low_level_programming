54585c0faa6127ef2a411d33772ec639f64295ed
Abdalmohsen-H <egywebdes@gmail.com>
2023-03-21 10:25:31 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _abs - function prints the absolute value abs
+ * @n: is int for the input number
+ * Return: always 0 (Success)
+ *
+ */
+int _abs(int n);
+{
+	if (n >= 0)
+	{
+		_putchar(n);
+	}
+	else
+	{
+		y = -1 * n
+		_putchar(y);
+	}
+	return (0);
+}
