55be97f360c360ba61b39613c2a4ad76ff1be663
Abdalmohsen-H <egywebdes@gmail.com>
2023-03-21 08:58:55 -0700

    prints the alphabet, in lowercase, followed by a new line.

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,18 +8,18 @@
 int main(void)
 {
 	/*this takes the first char of the string*/
-	char *wor = "_putchar";
-	/**
-	 * another way for this task char sh[] = {'_', 'p', 'u', 't', 'c'
-	 * , 'h', 'a', 'r', '\0'}; where '\0' stand for string ended
-	 */
-	while (*wor)
+	char *wor = "abcdefghijklmnopqrstuvwxyz";
+	void print_alphabet(void)
 	{
-		_putchar(*wor);
-		/* increments pointer variable "wor" to point to next character in string */
-
-		wor++;
+		while (*wor)
+		{
+			_putchar(*wor);
+			/* increments pointer variable "wor" to point to next character in string */
+	
+			wor++;
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
+	print_alphabet();
 	return (0);
 }
