4c1b58e5b587244c90c50aa500161308ce53538f
Abdalmohsen-H <egywebdes@gmail.com>
2023-03-22 06:52:25 -0700

    debug +ve , -ve with zero case

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - Debug positive_or_negative() function,
+ * test case when input equal to 0
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int test_value = 0;
+
+	positive_or_negative(test_value);
+	return (0);
+}
