e4b58da6ef6a7278d4c4295a3a61bae2befd44ce
Abdalmohsen-H <egywebdes@gmail.com>
2023-03-22 07:04:49 -0700

    follow instruction for task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,16 @@
 #include "main.h"
+
 /**
- * main - Debug positive_or_negative() function,
- * test case when input equal to 0
- * Return: Always 0 (Success)
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
 int main(void)
 {
 	int i;
 
 	i = 0;
 	positive_or_negative(i);
+
 	return (0);
 }
