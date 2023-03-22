78fb8b02c987f04e12cbba5d445fdf31bb6f17e3
Abdalmohsen-H <egywebdes@gmail.com>
2023-03-21 17:24:20 -0700

    first 50 Fibonacci numbers, starting with 1 and 2

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	long long y[] = {1, 2};
+	int count;
+
+	printf("1, 2, ");
+	for (count = 2 ; count < 51 ; count++)
+	{
+		y[count] = y[count - 1] + y[count - 2];
+		printf("%lld", y[count]);
+		if (count < 49)
+			printf(", ");
+	}
+	putchar('\n');
+	return (0);
+}
