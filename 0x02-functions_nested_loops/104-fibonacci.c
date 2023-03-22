171389c3f1325091f1ad1a6086cebad13b780d85
Abdalmohsen-H <egywebdes@gmail.com>
2023-03-21 18:05:45 -0700

    first 98 Fibonacci numbers, starting with 1 and 2

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	unsigned int a = 1, b = 2, c;
+	unsigned long long a = 1, b = 2, c;
 	int count;
 
 	printf("1, 2");
@@ -15,7 +15,7 @@ int main(void)
 		c = a + b;
 		a = b;
 		b = c;
-		printf(", %u", c);
+		printf(", %llu", c);
 	}
 	printf("\n");
 	return (0);
