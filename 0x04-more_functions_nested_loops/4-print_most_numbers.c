c6968641db390ec6d28b39684283483037a7474e
Abdalmohsen-H <egywebdes@gmail.com>
2023-03-23 20:07:44 -0700

    numbers except 2,4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints numbersexcept 2,4
+ * Return: void
+ *
+ */
+void print_most_numbers(void);
+{
+	char x = '0';
+
+	while (x <= '9')
+	{
+		if ('x' != 2 && x!= '4')
+			_putchar(x);
+		x++;
+	}
+	_putchar('\n');
+}
