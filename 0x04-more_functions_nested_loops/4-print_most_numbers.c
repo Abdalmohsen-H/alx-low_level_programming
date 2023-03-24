a3a1b1fd86e74564e9f74f00b750e77622fbfe8e
Abdalmohsen-H <egywebdes@gmail.com>
2023-03-23 20:29:46 -0700

    numbers except 2, 4 fixed

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,12 +6,12 @@
  */
 void print_most_numbers(void)
 {
-	char x = '0';
+	int x = 0;
 
-	while (x <= '9')
+	while (x <= 9)
 	{
-		if (x != '2' && x != '4')
-			_putchar(x);
+		if (x != 2 && x != 4)
+			_putchar(x + '0');
 		x++;
 	}
 	_putchar('\n');
