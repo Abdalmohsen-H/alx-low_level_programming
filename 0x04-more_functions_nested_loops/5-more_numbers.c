5c4caa8b42e6755abc9d1f8b17046fc571be7d0a
Abdalmohsen-H <egywebdes@gmail.com>
2023-03-24 19:52:51 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include <main.h>
+/**
+ * more_numbers - Entry point
+ * prints 0 to 14 , 14 time
+ * Return: Always 0 (Success)
+ */
+void more_numbers(void)
+{
+int row, d;
+
+for (row = 0 ; row < 15 ; row++)
+{
+for (d = 0 ; d < 15 ; d++)
+{
+if (d > 9)
+putchar(d / 10 + '0');
+putchar(d % 10 + '0');
+}
+putchar('\n');
+}
+return (0);
+}
