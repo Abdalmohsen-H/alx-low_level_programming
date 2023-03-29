f69f338ddc658ef233cdde8b084755cc8ac0c3d1
Abdalmohsen-H <egywebdes@gmail.com>
2023-03-28 19:59:10 -0700

    random words generator

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - random words generator
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+/* all available chars to use */
+char charset[] = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+/* Maximum length of generated words */
+int max_length = 10;
+/* hold generated word, +1 to hold '\0' */
+char word[max_length + 1];
+/* define number of rand words*/
+int count_rand_words;
+/*length of generated word*/
+int randlength;
+
+/* 
+* to ensure different numbers generated
+* each time the code runs link
+*random number generator with current time
+*/
+srand(time(NULL));
+
+/* Generate 50 random words */
+for (count_rand_words = 1; count_rand_words <= 50; count_rand_words++)
+{
+/* Generate a random length for 
+*rand word between 1 and MAX_LENGTH
+*/
+randlength = (rand() % max_length) + 1;
+/* Generate random letters for the word
+* with random length given above
+*/
+for (int j = 0; j < randlength; j++)
+{
+word[j] = charset[rand() % 62];
+}
+
+/* declare the word end
+* by adding '\0' to word
+*/
+word[randlength] = '\0';
+
+/* Print the generated word */
+printf("Word %d: %s\n", count_rand_words, word);
+}
+
+return (0);
+}
