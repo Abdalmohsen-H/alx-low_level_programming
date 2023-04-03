5f6b161bc167fa78be3a764940386dc0da1239df
Abdalmohsen-H <egywebdes@gmail.com>
2023-04-02 19:59:46 -0700

    sgs git push :

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_number - func Prints any integer.
+ * @n: input integer.
+ */
+void print_number(int n)
+{
+unsigned int num = n;
+int div = 10;
+
+if (n < 0)
+{
+_putchar('-');
+num = -num;
+}
+while ((num / div) >= 1)
+{
+div *= 10;
+}
+div = div / 10;
+while (div > 0)
+{
+_putchar((num / div + '0');
+num = num % div;
+div = div / 10;
+}
+}
