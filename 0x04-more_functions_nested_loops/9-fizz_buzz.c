4527e28aa1cce2eae0fdc5de0224914ecd0f4713
Abdalmohsen-H <egywebdes@gmail.com>
2023-04-02 19:25:03 -0700

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - fuzz buzz numbers
+ *
+ * Return: Always 1.
+ */
+int main(void)
+{
+int n;
+
+for (n = 1;  n <= 100; n++)
+{
+if (n % 3 == 0 && n % 5 == 0)
+	printf("FizzBuzz\t");
+else if (n % 3 == 0)
+	printf("Fizz\t");
+else if (n % 5 == 0)
+	printf("Buzz\t");
+else
+	printf("%d\t", n);
+}
+printf('\n');
+return (1);
+}
