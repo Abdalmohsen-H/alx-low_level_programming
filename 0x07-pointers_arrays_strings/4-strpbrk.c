bfd2ba1b18caa2d1e68be2ab906e66012f36d0df
Abdalmohsen-H <egywebdes@gmail.com>
2023-04-04 09:10:21 -0700

    4-strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+* _strpbrk - locate any match of accept
+* letters in sting
+* @s: input string
+* @accept: search chars
+* Return: pointer to byte in s found
+* if found in accept, else NULL
+*/
+char *_strpbrk(char *s, char *accept)
+{
+int found = 0;
+int ctr = 0;
+int ctr2;
+
+while (*(s + ctr) != '\0')
+{
+for (ctr2 = 0; *(accept + ctr2) != '\0'; ctr2++)
+{
+if (*(s+ctr) == *(accept + ctr2))
+{
+found = 1;
+/*printf("Match found: %c\n", *(s + ctr));*/
+/*printf("found = %d\n", found);*/
+
+return (s + ctr);
+}
+/*printf("No match: %c\n", *(s+ctr));*/
+/*printf("found = %d\n", found);*/
+}
+if (found == 1)
+	break;
+ctr++;
+}
+if (found == 0)
+	return ('\0');
+/*printf("Final count: %d\n", found);*/
+return (s+ctr);
+}
