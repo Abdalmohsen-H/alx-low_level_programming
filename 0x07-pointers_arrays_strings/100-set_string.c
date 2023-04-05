fd161ebb431ce70fd4da33a706d26c0f3575f7d4
Abdalmohsen-H <egywebdes@gmail.com>
2023-04-04 19:32:20 -0700

    using double pointer to sets the value of a pointer to a char.

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#iclude "main.h"
+/**
+ * set_string - double pointer
+ *
+ * @s: double pointer
+ * @to: pointer
+ */
+void set_string(char **s, char *to)
+{
+/*removed extr * because it means derefernce*
+*s = to;
+}
