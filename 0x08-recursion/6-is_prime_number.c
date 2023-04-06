7a229934137a15ad6b7f740e7da5c3ecce38610d
Abdalmohsen-H <egywebdes@gmail.com>
2023-04-05 23:22:14 -0700

    prime number detector using recursion

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+* is_prime_number - detect prime numbers
+* @n: input number.
+* @base: random num to check
+* Return: 1 if prime number, else 0.
+*/
+int check_prime_number(int n, int base);
+
+int is_prime_number(int n)
+{
+int base;
+if (n <= 1)
+{
+return (0);
+}
+if (n > 1000)
+{
+base =  (n / 4);
+}
+else
+{
+base = (n / 2);
+}
+return (check_prime_number(n, base));
+}
+/**
+ * check_prime_number - helper function
+ * @base: random number to check
+ * @n:input number
+ * Return: 1 if prime num ,else 0
+ */
+int check_prime_number(int n, int base)
+{
+int x;
+x = n % base;
+
+if (x == 0 && base > 1)
+{
+/* not a prime number*/
+return (0);
+}
+if (base <= 1)
+{
+return (1);
+}
+base--;
+return (check_prime_number(n, base));
+}
