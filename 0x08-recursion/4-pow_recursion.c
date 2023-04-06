5b51cdb84659dfabc9fee509585ba1df07276139
Abdalmohsen-H <egywebdes@gmail.com>
2023-04-05 21:50:07 -0700

    x ^ y  -calcualte x to the power y with recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* factorial - Calculate base n raised
+* to a power y, AKA (x ^ y).
+* @x: input base number.
+* @y: input power number.
+* Return: x ^ y.
+*/
+int _pow_recursion(int x, int y)
+{
+int base = 1;
+if (y > 0)
+{
+base *= _pow_recursion(x, (y - 1));
+return (base *x);
+}
+else if (y < 0)
+{
+return (-1);
+}
+/* if y == 0 */
+return (1);
+}
