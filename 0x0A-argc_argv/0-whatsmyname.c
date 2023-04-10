f319dfd83df535ad2bfad6d79c5370f6eb597dfc
Abdalmohsen-H <egywebdes@gmail.com>
2023-04-09 21:33:51 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints its name
+ * @argc: argument counter
+ * @argv: argument values
+ * Return: 0
+ */
+/* int main(int *argc, char **argv) */
+int main(int *argc, char *argv[])
+{
+(void) argc;
+printf("%s\n", argv[0]);
+return (0);
+}
