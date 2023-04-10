d29f77256165dce7b22b4143a27103530ec172ef
Abdalmohsen-H <egywebdes@gmail.com>
2023-04-09 21:38:25 -0700

    task 0 - function prints its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,8 +5,9 @@
  * @argv: argument values
  * Return: 0
  */
+/* int main(int *argc __attribut__((unused)), char **argv) */
 /* int main(int *argc, char **argv) */
-int main(int *argc, char *argv[])
+int main(int argc, char *argv[])
 {
 (void) argc;
 printf("%s\n", argv[0]);
