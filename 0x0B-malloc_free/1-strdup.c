5b3afc28607ad068c80d2a54aace17915a1d8720
Abdalmohsen-H <egywebdes@gmail.com>
2023-04-11 19:29:45 -0700

    task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,9 +1,9 @@
-#include "main.h"
+//#include "main.h"
 #include <stdlib.h>
-/* #include <stdio.h>*/
+#include <stdio.h>
 /* #include <string.h>*/
 /**
- * _strdup - copy input string on new
+ * create_array - copy input string on new
  * allocated memory space
  * @str: array size
  * Return: pointer to new space, Else NULL
@@ -11,26 +11,28 @@
 char *_strdup(char *str)
 {
 char *outarr;
-int length = 0, i = 0;
+int length =0, i = 0;
 
+if (str == NULL || outarr == NULL)
+{
+return (NULL);
+}
 /* length = strlen(str);*/
-while (*(str + i) != '\0')
+while(*(str+i) != '\0')
 {
 i++;
 length++;
 }
 i = 0;
 outarr = malloc((length + 1) * sizeof(char));
-if (str == NULL || outarr == 0 || outarr == NULL || length == 0)
+if (outarr == NULL)
 {
 return (NULL);
 }
-while (length > 0)
+while (length > 0 && *str != '\0')
 {
 outarr[i] = *str;
 str++;
 length--;
 i++;
-}
-return (outarr);
 }
