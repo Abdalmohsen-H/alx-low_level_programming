9dc563c3c6d1375cf5d79641390b6dbd27a9a557
Abdalmohsen-H <egywebdes@gmail.com>
2023-04-11 19:37:10 -0700

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,6 +14,10 @@ char *str_concat(char *s1, char *s2)
 char *outarr;
 int length1 = 0,length2 = 0, i = 0;
 
+if (s1 == NULL && s2 ==NULL)
+{
+return (NULL);
+}
 length1 = strlen(s1);
 length2 = strlen(s2);
 outarr = malloc((length1 + length2 + 1) * sizeof(char));
