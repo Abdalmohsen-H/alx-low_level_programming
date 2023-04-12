2d1bebcf030dd4f5c1061313f4da53d46fb803bc
Abdalmohsen-H <egywebdes@gmail.com>
2023-04-11 18:04:18 -0700

    task 1

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+/* #include <stdio.h>*/
+/* #include <string.h>*/
+/**
+ * create_array - copy input string on new
+ * allocated memory space
+ * @str: array size
+ * Return: pointer to new space, Else NULL
+ */
+char *_strdup(char *str)
+{
+char *outarr;
+int length =0, i = 0;
+
+/* length = strlen(str);*/
+while(*(str+i) != '\0')
+{
+i++;
+length++;
+}
+i = 0;
+outarr = malloc((length + 1) * sizeof(char));
+if (str == NULL || outarr == NULL)
+{
+return (NULL);
+}
+while (length > 0 && *str != '\0')
+{
+outarr[i] = *str;
+str++;
+length--;
+i++;
+}
+return (outarr);
+}
