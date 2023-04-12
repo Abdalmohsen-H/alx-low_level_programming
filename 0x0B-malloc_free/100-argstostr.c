cfcc4cac85b1bf370f98cae2d66d7e5419944ce9
Abdalmohsen-H <egywebdes@gmail.com>
2023-04-11 19:57:35 -0700

    task 100

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdlib.h>
+/* #include <stdio.h> */
+#include <string.h>
+/**
+ * argstostr -  concatenates all arguments
+ * on new allocated memory space
+ * @ac: argc
+ * @av: argv
+ * Return: pointer to new space, Else NULL
+ */
+char *argstostr(int ac, char **av);
+{
+char *outarr;
+int length1 = 0, length2 = 0, i = 0, j;
+int size = 1;
+
+if (ac == 0 || av == NULL)
+return (NULL);
+for (j = 1; j < ac; j++)
+{
+if (av[j] == NULL)
+	av[j] = "\0";
+size += strlen(av[j]);
+}
+outarr = malloc(size *sizeof(char));
+if (outarr == NULL)
+{
+return (NULL);
+}
+for (j = 1; j < ac; j++)
+{
+while (strlen(av[j]) > 0 && *s1 != '\0')
+{
+outarr[i] = *s1;
+s1++;
+length1--;
+i++;
+}
+outarr[i] = '\n';
+}
+return (outarr);
+}
