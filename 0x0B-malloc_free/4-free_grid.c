b5d532e19e835ab071894556e6f952f8729b4ec3
Abdalmohsen-H <egywebdes@gmail.com>
2023-04-12 20:07:35 -0700

    4- free grid fixed

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdlib.h>
 /**
  * free_grid - frees a 2D array
  * @grid : input
@@ -10,7 +10,7 @@ void free_grid(int **grid, int height)
 {
 int ctr;
 
-for (ctr = 0; ctr < height, ctr ++)
+for (ctr = 0; ctr < height; ctr ++)
 {
 free(grid[ctr]);
 }
