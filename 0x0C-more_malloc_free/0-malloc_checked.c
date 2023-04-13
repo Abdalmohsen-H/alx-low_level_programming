9540839b23c66de74b25e12b089e151e7cedab37
Abdalmohsen-H <egywebdes@gmail.com>
2023-04-13 09:13:47 -0700

    task 0 malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - allocates memory using malloc
+ * and checking if it fails or not
+ * @b: given int
+ * Return: pointer to allocated memory or NULL
+*/
+void *malloc_checked(unsigned int b)
+{
+void *reserv;
+
+reserv = malloc(b);
+if (!reserv)
+{
+return (98);
+}
+return (reserv);
+}
