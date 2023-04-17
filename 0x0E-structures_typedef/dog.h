d80db27765e739a3364caf505a5b4ba93fc8279a
Abdalmohsen-H <egywebdes@gmail.com>
2023-04-17 12:50:19 -0700

    task 0 , define struct dog  on custom header

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef custom_h0
+#define custom_h0
+
+/**
+* struct dog - define struct dog
+*@name - dog name
+*@age - dog age
+*@owner - owner name
+*Description: long desc.
+*/
+
+struct dog
+{
+	ch *name;
+	float age;
+	ch *owner;
+};
