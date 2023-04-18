b551b52f7d0df9213027ea5f18bca9681726d4c7
Abdalmohsen-H <egywebdes@gmail.com>
2023-04-18 07:21:50 -0700

    task 5 free a dog struct

---
+++ 0x0E-structures_typedef/5-freedog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * free_dog - print struct dog
+ * @d: pointer for dog struct
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		/* different ways to access struct pointers element*/
+		if (d->name)
+			free((*d).name);
+		if (d->age)
+			free(d-<age);
+		if ((*d).owner)
+			free(d->owner);
+		free(d);
+	}
+}
