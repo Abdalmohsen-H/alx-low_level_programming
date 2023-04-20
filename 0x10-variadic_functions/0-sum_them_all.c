8ffeccaf7c5a12c8f7919f0cf002632bb08bc701
Abdalmohsen-H <egywebdes@gmail.com>
2023-04-20 11:41:40 -0700

    variadic functions task 0 (sum all int arguments)

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,30 @@
+#include <stdarg.h>
+#include <stdio.h>
+/**
+* sum_them_all - returns sum of all inputs (args)
+* @n: count of args
+* @...: accept unknown number of inputs
+* Return: integer sum of all inputs
+*/
+int sum_them_all(const unsigned int n, ...)
+{
+va_list getargs;
+int sum = 0;
+int counter = n;
+
+/* if n==0 return 0*/
+if (!n)
+{
+return (0);
+}
+/* intialiaze getargs list*/
+va_start(getargs, n);
+for (; counter > 0; counter--)
+{
+/* va_arg(argument_list, arg_type) */
+sum += va_arg(getargs, int);
+}
+/* free getargs*/
+va_end(getargs);
+return (sum);
+}
