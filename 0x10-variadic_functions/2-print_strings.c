26753636c4f7b2b98f0dde69bb714484244b1afd
Abdalmohsen-H <egywebdes@gmail.com>
2023-04-20 13:01:07 -0700

    task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -11,6 +11,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 va_list getargs;
 int counter;
+char *temp;
 
 /* if n==0 return*/
 if (!n)
@@ -22,8 +23,9 @@ return;
 va_start(getargs, n);
 for (counter = n; counter > 0; counter--)
 {
-if (getargs != '\0')
-{printf("%s", va_arg(getargs, char *));
+temp = va_arg(getargs, char *);
+if (temp)
+{printf("%s", temp);
 }
 else
 {printf("(nil)");
