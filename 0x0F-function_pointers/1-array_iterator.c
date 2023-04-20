cb65ffd7faccf9a48129db5a26cefa4dfa87c13a
Abdalmohsen-H <egywebdes@gmail.com>
2023-04-19 19:50:03 -0700

    task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,4 +1,6 @@
+#include <stddef.h>
 #include "function_pointers.h"
+#include <stdlib.h>
 /**
  * array_iterator - func pointer takes
  * input from array of inputs
@@ -11,7 +13,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 int i;
 if (array && size && action)
-for (i = 0; i < size; i++)
-{action(*array[i]);
+for (i = 0; i < (int) size; i++)
+{action(array[i]);
 }
 }
