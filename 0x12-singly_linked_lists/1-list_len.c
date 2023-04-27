4d90c4c73d4789fcddf2d9b39695fe2b85290a76
Abdalmohsen-H <egywebdes@gmail.com>
2023-04-27 04:15:07 -0700

    task 1 - return number of nodes only

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * print_list - prints all nodes data &
- * return list length (AKA number of nodes)
+ * print_list - return list length (AKA number of nodes)
  *
  * @h: pointer to head of linked list (first node)
  * this is very important to define this list
@@ -14,23 +13,14 @@
  * size_t ensure we have largest size for object
  * in the system
  */
-size_t print_list(const list_t *h)
+size_t list_len(const list_t *h)
 {
 	size_t nodecount = 0;
-	char *string;
-	unsigned int length;
 
 	/* condithion h, means if node exist (True)*/
-        /* then loop linked list again, still unfinished*/
+	/* then loop linked list again, still unfinished*/
 	for (; h; nodecount++)
 	{
-		string = h->str;
-		length = h->len;
-		if (!(h->str))
-		{string = "(nil)";
-			length = 0;
-		}
-		printf("[%d] %s\n", length, string);
 		h = h->next;
 	}
 	return (nodecount);
