9b6e0f286e61fcb89f1f531b536da3ced1f1415c
Abdalmohsen-H <egywebdes@gmail.com>
2023-04-27 18:54:22 -0700

    task 6 asm

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,18 @@
+section .data
+    ; Define a message string with a newline and null terminator
+    my_message db 'Hello, Holberton', 10, 0
+
+section .text
+    ; Declare the printf function as external
+    extern printf
+    ; Declare the entry point of the program
+    global main_func
+
+main_func:
+    ; Load the address of the message string into the EDI register
+    mov edi, my_message
+    ; move Zero into EAX register which represent the 0 return of printf
+    mov eax, 0
+    ; Call the printf function with the message string as an argument
+    call printf
+
