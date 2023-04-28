633cf1b7b10718ca3b2945b1f6f1c9e0c4549430
Abdalmohsen-H <egywebdes@gmail.com>
2023-04-27 17:00:41 -0700

    chnges on lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,9 @@
 #ifndef linklist_h
 #define linklist_h
 
+#include <stddef.h>
+#include <stdlib.h>
+
 /**
  * struct list_s - singly linked list node defined as structure
  * @str: string - (malloc'ed string) malloc used to
@@ -24,5 +27,5 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-
+int _putchar(char c);
 #endif
