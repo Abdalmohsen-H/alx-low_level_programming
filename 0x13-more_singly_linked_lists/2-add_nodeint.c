cd4accbd70922b5c1524a2a57e92dca783705608
Abdalmohsen-H <egywebdes@gmail.com>
2023-04-29 21:13:52 -0700

    task 2 test

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * add_nodeint - add node at beginning of singly linked list
+ * @n: integer
+ * @head: pointer of pointer to head node
+ *
+ * Return: the address of the new node
+ * or NULL if it failed
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+int idx = 0;
+list_t *newnode;
+
+if (str == NULL || !head)
+	return (NULL);
+newnode->n = n;
+newnode->next = *head;
+*head = newnode;
+return (newnode);
+}
