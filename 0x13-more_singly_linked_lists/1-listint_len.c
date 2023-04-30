da31213a2a831171bbddba0741a2afe2f5626d66
Abdalmohsen-H <egywebdes@gmail.com>
2023-04-29 21:08:51 -0700

    task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * listint_len - return list length (AKA number of nodes)
+ *
+ * @h: pointer to head of linked list (first node)
+ * this is very important to define this list
+ * as you always need to know first node to access
+ * the linked list and get nest nodes
+ *
+ * Return: reurn number of nodes in linked list
+ * of type (size_t) to ensure we have sufficient size
+ * size_t ensure we have largest size for object
+ * in the system
+ */
+size_t listint_len(const listint_t *h);
+{
+size_t nodecount = 0;
+
+/* condithion h, means if node exist (True)*/
+/* then loop linked list again, still unfinished*/
+for (; h; nodecount++)
+{
+h = h->next;
+}
+return (nodecount);
+}
