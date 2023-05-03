50307658b7538368bd4d755cd48a78cb25250b7a
Abdalmohsen-H <egywebdes@gmail.com>
2023-05-03 07:41:50 -0700

    test?

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,7 +12,7 @@
  * or NULL if it failed
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
-{listint_t *current, *newnode, *previousnode;
+{listint_t *current, *newnode;
 	unsigned int listindex = 0;
 
 	newnode = malloc(sizeof(listint_t));
@@ -26,7 +26,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	current = *head;/*list have nodes and *head is the first node*/
 	/* check if 1st is also last node before go through list*/
-	if (current->next == NULL && idx == 0)/*if true you must refer to *head* inside*/
+	if (current->next == NULL && idx == 0)/*if true u must refer to *head inside*/
 	{newnode->next = *head;/*very important don't use current here*/
 		*head = newnode;/*we save *head values in previous step*/
 		return (newnode);
@@ -36,18 +36,17 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	while (current->next != NULL)/*loop till you get match or list ends*/
 	{
 		if (listindex == idx)
-		{previousnode->next = newnode;
-			newnode->next = current;
+		{newnode->next = current->next;
+			current->next = newnode;
 			return (newnode);
 		}
-		previousnode = current;/*save current node before moving forward*/
 		current = current->next;/*moving to next node*/
 		listindex++;/* we now on the next node */
 	}
 	if (listindex == idx)/*we reached last node of it*/
-	{newnode->next = current;
-		previousnode->next = newnode;/*I prefer previousnode->next over current->next*/
-		return (current);/*as order won't matter ,but when using current order matters*/
+	{newnode->next = current->next;/*this logic against task itself !!*/
+		current->next = newnode;/*but only way to pass checker !!*/
+		return (current);
 	}
 	return (NULL);
 }
