9f4c22369ba96725b27b0dd2a98daa88cfe62c54
Abdalmohsen-H <egywebdes@gmail.com>
2023-05-02 18:53:31 -0700

    task 3 3. Add node at the end

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * add_nodeint_end - add node to end of singly linked list
+ * AKA addind newnode to linked list's tail
+ * @n: integer
+ * @head: pointer of pointer to head node
+ *
+ * Return: the address of the new node
+ * or NULL if it failed
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *newnode;
+
+	newnode = malloc(sizeof(listint_t));
+	if (newnode == NULL)
+		return (NULL);
+	newnode->n = n;
+	if (*head == NULL) /*list still NULL (no nodes)*/
+	{
+		*head = newnode;/*point head to first newnode*/
+		newnode->next = NULL;/*first newnode is also lastnode*/
+		/*till we add extra nodes*/
+	}
+	else /*the list already have nodes*/
+	{listint_t *current = *head;/*then *head is the first node*/
+		/* check if this lsat node in list before add newnode*/
+		while (current->next != NULL)
+		{/* then loop till you go to last node of List*/
+			/* AKA loop to traverse a linked list data structure.*/
+			current = current->next;
+			/* we now on the next node */
+		}
+		/*loop ended when we reached last node of it*/
+		/* now we can add new node at the end of list*/
+		current->next = newnode;
+		/* new added at the end of linked list*/
+	}
+	return (newnode);
+}
