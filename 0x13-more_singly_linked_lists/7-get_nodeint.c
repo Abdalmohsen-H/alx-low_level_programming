daf76d5a5e998e63d352ab89ac880168fca6a788
Abdalmohsen-H <egywebdes@gmail.com>
2023-05-03 03:41:48 -0700

    task 7, check if given index exist in List and return its data

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,55 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * get_nodeint_at_index - find if index exist on
+ * singly linked list and return matching node
+ * index's data
+ * @index: input index
+ * @head: pointer to head node
+ *
+ * Return: matching node index's data
+ * or NULL if it failed
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *newnode;
+	listint_t *current;
+	unsigned int listindex = 0;
+
+	if (*head == NULL || idx < 0) /*list still NULL (no nodes)*/
+	{
+		return (NULL);
+	}
+	else /*the list already have nodes*/
+	{current = *head;/*then *head is the first node*/
+		/* check if 1st is also last node before go through list*/
+		if (current->next == NULL && idx == 0)
+		{
+			return (current->n);
+		}
+		else if (current->next == NULL && idx > 0)
+		{/* index not found on list return null*/
+			return (NULL);
+		}
+		while (current->next != NULL)
+		{/* then loop till you get match or list ends*/
+			if (listindex == index)
+			{
+				return (current->n);
+			}
+			/* AKA loop to traverse a linked list data structure.*/
+			current = current->next;
+			listindex++;
+			/* we now on the next node */
+		}
+		/*loop ended when we reached last node of it*/
+		current->next = newnode;
+		if (listindex == index)
+		{	/* now check if index matches or not*/
+			return (current->n);
+		}
+	}
+	return (NULL);
+}
