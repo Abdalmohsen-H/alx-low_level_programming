70b9c33f4f7dd100ca2e8d8171a58d8812c8bf1c
Abdalmohsen-H <egywebdes@gmail.com>
2023-05-05 07:35:39 -0700

    task 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * get_bit - get bit at given index
+ * @n: input decimal number
+ * @index:index from left starting from 0
+ *
+ * Return: value of bit at matching index,
+ * else -1.
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int bitmask = 1;
+	unsigned int counter = 0;
+
+	/* If the input is zero, print a single zero and return */
+	if (n == 0 && index == 0)
+		return (0);
+	if (n == 0 && index != 0)
+		return (-1);
+
+	/* Loop through all the bits of input number*/
+	while (bitmask > 0)
+	{
+		if (index == counter)
+		{/* Check if the bit is set in imput number */
+			if ((n & bitmask))
+			{
+				/*printf("n & bitmask: %lu", n & bitmask);*/
+				return (1);
+			}
+			else
+			{
+				/*printf("n & bitmask: %lu", n & bitmask);*/
+				return (0);
+			}
+		}
+		/*Shift mask one bit to the left as index from left*/
+		bitmask = bitmask << 1;
+		counter++;
+	}
+	return (-1); /*index out of range*/
+}
