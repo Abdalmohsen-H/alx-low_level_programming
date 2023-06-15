34708cbd84c788434e24a3fdb15796098c8935aa
Abdalmohsen-H <egywebdes@gmail.com>
2023-06-15 11:20:20 -0700

    task 0 fix betty , add checking conditions for special cases

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
 #include "lists.h"
 /**
-  *print_dlistin - print data off nodes
-  * in doubly linked list
-  *@h: head of D lnkdlist
-  *
-  *Return: num of nodes found
-  */
+ *print_dlistint - print data off nodes
+ * in doubly linked list
+ *@h: head of D lnkdlist
+ *or just node of list you have to check
+ *
+ *Return: num of nodes found
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
 	const dlistint_t *headtmp = h;
 	size_t cntr = 0;
 
+	if (headtmp == NULL)
+		return (cntr);/*return 0*/
+	while (headtmp->prev != NULL)
+	{headtmp = headtmp->prev
+	}
 	while (headtmp->next)
 	{
 		printf("%d\n", headtmp->n);
