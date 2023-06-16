3dcabab4b9add99b60a7ca42741ee58b909de502
Abdalmohsen-H <egywebdes@gmail.com>
2023-06-15 19:02:35 -0700

    task 8 wip

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -11,41 +11,39 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {dlistint_t *current = NULL, *node = NULL, *previous_node = NULL;
-    unsigned int listindex = 0;
+	unsigned int listindex = 0;
 
-    if  (head == NULL || *head == NULL)
-    {
-        return (-1);
-    }
-    current = *head;/*list have nodes and *head is the first node*/
-    if (index == 0)/*if true u must refer to *head inside*/
-    {
-        *head = (*head)->next;/*move *header to next node*/
-        (*head)->prev = NULL;
-        if (current)
+	if  (head == NULL || *head == NULL)
+	{
+		return (-1);
+	}
+	current = *head;/*list have nodes and *head is the first node*/
+	if (index == 0)/*if true u must refer to *head inside*/
+	{
+		*head = (*head)->next;/*move *header to next node*/
+		(*head)->prev = NULL;
 		free(current);
-        return (1);
-    }
-    else if ((*head)->next == NULL && index > 0)/* index not found on list*/
-        return (-1);
-    while (current != NULL)
-    {/*loop till doublelist ends*/
-        if (listindex == index)/*node matching index*/
-        {
-            node = *head;/*we save *head node to free it*/
-            if (node->prev != NULL)
-            {previous_node = node->prev;
-              previous_node->next = current->next;/*order of this block matters*/
-            }
-            if (current->next != NULL)
-                (current->next)->prev = previous_node;
-            if (node)
-		    free(node);/*this should happen in this order*/
-            return (1);/*to avoid losing data*/
-        }
-        current = current->next;
-        listindex++;/* we now on the next node */
-    }
-    /*loop ended without match, then index out of list range*/
-    return (-1);
+		return (1);
+	}
+	else if ((*head)->next == NULL && index > 0)/* index not found on list*/
+		return (-1);
+	while (current != NULL)
+	{/*loop till doublelist ends*/
+		if (listindex == index)/*node matching index*/
+		{
+			node = current;/*we save *head node to free it*/
+			if (node->prev != NULL)
+			{previous_node = node->prev;
+				previous_node->next = current->next;/*order of this block matters*/
+			}
+			if (current->next != NULL)
+				(current->next)->prev = previous_node;
+			free(node);/*this should happen in this order*/
+			return (1);/*to avoid losing data*/
+		}
+		current = current->next;
+		listindex++;/* we now on the next node */
+	}
+	/*loop ended without match, then index out of list range*/
+	return (-1);
 }
