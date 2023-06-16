bfa7bd599a171a2fbaea3a8ece6b4e61287bfdf8
Abdalmohsen-H <egywebdes@gmail.com>
2023-06-15 18:00:38 -0700

    task 7 handle if no node exist and free malloce'd newnod if index not exist

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,13 +13,15 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {unsigned int cntr = 0;
 	dlistint_t *head = *h;
 	dlistint_t *newnod;
-
+    
+    if (head == NULL && idx != 0)
+        return (NULL);
 	newnod = (dlistint_t *) malloc(sizeof(dlistint_t));
 	if (newnod == NULL)
 		return (NULL);
 	if (head == NULL)
 		return (NULL);
-	if (idx == 0)
+	if (idx == 0 || head == NULL)
 		return (add_dnodeint(h, n));
 	if (head->next == NULL)
 		return (add_dnodeint_end(h, n));
@@ -38,5 +40,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	} while (head->next != NULL);
 	if (idx == cntr)
 		return (add_dnodeint_end(h, n));
+	free(newnod);	
 	return (NULL);
 }
