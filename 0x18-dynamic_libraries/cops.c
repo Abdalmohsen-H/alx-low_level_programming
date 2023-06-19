e0d81266a29a31d694948aeea4c1359d9fd02648
Abdalmohsen-H <egywebdes@gmail.com>
2023-06-18 21:35:51 -0700

    task 100 advanced

---
+++ 0x18-dynamic_libraries/cops.c
@@ -0,0 +1,58 @@
+/**
+ * sub - subtract func.
+ * @x: 1st input
+ * @y: 2nd input
+ *
+ * Return: sub of 2 inputs
+ */
+int sub(int x, int y)
+{
+	return (x - y);
+}
+
+/**
+ * add - add tow inputs
+ * @h: int 1st input
+ * @k: 2nd num
+ *
+ * Return: sum of h and k
+ */
+int add(int h, int k)
+{int res;
+	res = h + k;
+	return (res);
+}
+
+/**
+ * mul - result of (1st num * 2nd num)
+ * @t: 1st num
+ * @y: 2nd integer
+ *
+ * Return: multiply t*y
+ */
+int mul(int t, int y)
+{
+	return (t * y);
+}
+/**
+ * mod - h % j
+ * @h: num input
+ * @j: int input
+ *
+ * Return: int as result
+ */
+int mod(int h, int j)
+{
+	return (h % j);
+}
+/**
+ * div - v / b
+ * @v: 1st input
+ * @b: num
+ *
+ * Return: res of divide opration
+ */
+int div(int v, int b)
+{
+	return (v / b);
+}
