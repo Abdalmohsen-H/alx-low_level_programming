4100830c98ca73e35f40d0d5ae5ab679e48eb9c5
Abdalmohsen-H <egywebdes@gmail.com>
2023-07-07 13:06:35 -0700

    task 3 wip

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "hash_tables.h"
+/**
+ * hash_table_set - hash_table_set
+ *@ht: ht
+ *@key: key
+ *@value: val
+ *Return: int 1 or 0
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{unsigned long int hash;
+	hash_node_t *tmpnode, *nw_node =  malloc(sizeof(hash_node_t));
+	int idx;
+
+	if (key == NULL || *key == '\0' || ht == NULL || value ==NULL)
+		return(0);
+	hash = key_index(key, ht->size);
+
+	/*printf("ky:%i",sizeof(ht->array[hash]->key));*/
+
+	tmpnode = ht->array[hash];
+	/*while(tmpnode != NULL)*/
+	while(tmpnode->key != NULL)
+	{
+		if(strcmp(tmpnode->key, key) == 0)
+		{printf("key already exist update value");
+			tmpnode->value = strdup(value);
+			return (1);
+		}
+		if(tmpnode->next != NULL)
+			tmpnode = tmpnode->next;
+		else
+			break;
+	}
+
+	nw_node =  malloc(sizeof(hash_node_t));
+	if (nw_node == NULL)
+		return (0);
+
+
+	nw_node->key = strdup(key);
+	nw_node->value = strdup(value);
+	ht->array[hash]->next = NULL;
+
+	if (ht->array[hash]->key != NULL)
+	{nw_node->next = ht->array[hash];
+		ht->array[hash] = nw_node;  
+	}
+	else
+		ht->array[hash] = nw_node;
+	return(1);
+}
