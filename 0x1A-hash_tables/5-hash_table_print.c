cc50c01e77d341747a40b92aff954222049bd26a
Abdalmohsen-H <egywebdes@gmail.com>
2023-07-07 14:33:14 -0700

    task 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "hash_tables.h"
+/**
+ * hash_table_print - print func hash_table_print
+ *  *@ht: ht
+ */
+void hash_table_print(const hash_table_t *ht)
+{hash_node_t *tmpnode;
+	if (ht != NULL)
+	{
+		while (tmpnode != NULL && tmpnode->key != NULL)
+		{
+			printf("'%s': '%s'", tmpnode->key, tmpnode->value);
+			if (tmpnode->next != NULL)
+				tmpnode = tmpnode->next;
+			else
+				break;
+		}
+	}
+}
