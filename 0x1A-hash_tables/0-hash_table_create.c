71b9f66e5e27e78858c31a1dc4dda7d0a7bd648c
Abdalmohsen-H <egywebdes@gmail.com>
2023-07-06 17:53:56 -0700

    task 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - create hashtable
+ * @size: input size for hash table
+ * Return: pointe to Hashtable
+ * if failed return Null
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+
+	htbl = malloc(sizeof(hash_table_t));
+	if (htbl == NULL)/* if failed */
+		return (NULL);
+	htbl->size = size;
+	return (htbl);
+}
