182f0c40dbd594eccba58c64853b844134ca2edc
Abdalmohsen-H <egywebdes@gmail.com>
2023-07-07 13:28:41 -0700

    task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,24 +12,20 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {unsigned long int hash;
 	hash_node_t *tmpnode, *nw_node =  malloc(sizeof(hash_node_t));
-	/*int idx;*/
 
-	if (key == NULL || *key == '\0' || ht == NULL || value ==NULL)
-		return(0);
+	if (key == NULL || *key == '\0' || ht == NULL || value == NULL)
+		return (0);
 	hash = key_index((const unsigned char *) key, ht->size);
 
-	/*printf("ky:%i",sizeof(ht->array[hash]->key));*/
-
 	tmpnode = ht->array[hash];
-	/*while(tmpnode != NULL)*/
-	while(tmpnode != NULL && tmpnode->key != NULL)
+	while (tmpnode != NULL && tmpnode->key != NULL)
 	{
-		if(strcmp(tmpnode->key, key) == 0)
+		if (strcmp(tmpnode->key, key) == 0)
 		{printf("key already exist update value");
 			tmpnode->value = strdup(value);
 			return (1);
 		}
-		if(tmpnode->next != NULL)
+		if (tmpnode->next != NULL)
 			tmpnode = tmpnode->next;
 		else
 			break;
@@ -39,12 +35,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (nw_node == NULL)
 		return (0);
 
-
 	nw_node->key = strdup(key);
 	nw_node->value = strdup(value);
-
+	/*could be null or collision node*/
 	nw_node->next = ht->array[hash];
-	ht->array[hash] = nw_node;  
-	
-	return(1);
+
+	ht->array[hash] = nw_node;
+
+	return (1);
 }
