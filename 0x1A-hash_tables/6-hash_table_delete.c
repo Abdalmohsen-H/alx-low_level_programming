ffd609ef5014c3b847c14544c70dbacb0176727a
Abdalmohsen-H <egywebdes@gmail.com>
2023-07-07 18:15:09 -0700

    t6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "hash_tables.h"
+/**
+ *hash_table_delete - delete hashtable
+ *@ht - hashtable
+ */
+void hash_table_delete(hash_table_t *ht);
+{hash_node_t *tmpnode;
+	unsigned long int idx;
+	if (ht != NULL)
+	{
+		for (idx = 0; idx < ht->size; idx++)
+		{tmpnode = ht->array[idx];
+			while (tmpnode != NULL && tmpnode->key != NULL)
+			{
+				free(tmpnode)
+					if (tmpnode->next != NULL)
+						tmpnode = tmpnode->next;
+					else
+						break;
+			}
+		}
+		ht->array = NULL;
+		free(ht)
+	}
+}
