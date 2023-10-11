8f9e2cad71254a4959995851159d62e08f58cb7a
Abdalmohsen-H <egywebdes@gmail.com>
2023-10-10 17:54:12 -0700

    task 1: nbinary search in c for a sorted array, fix betty

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -17,7 +17,7 @@ int binary_search(int *array, size_t size, int value)
 		{
 			mid = (rgtidx + lftidx) / 2;
 			/* print every iteration array */
-			printf("Searching in array:");
+			printf("Searching in array: ");
 			for (idx = lftidx; idx < rgtidx; idx++)
 			{
 				printf("%d, ", array[idx]);
@@ -34,7 +34,7 @@ int binary_search(int *array, size_t size, int value)
 			}
 			else /* array[mid] > value */
 			{
-				rgtidx = mid -1;
+				rgtidx = mid - 1;
 			}
 		}
 	}
